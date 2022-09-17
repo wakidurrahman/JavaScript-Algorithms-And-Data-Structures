4cf37a311c278d5a8fbfb2f0fcad2e98947afb8a
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-09-17 22:10:10 +0900

    message remove

--- javascript-today-magazine/bubble-sort.js
+++ javascript-today-magazine/bubble-sort.js
@@ -16,5 +16,5 @@ function handleBubbleSort(array) {
 var bubbleSortSample = [3, 4, 9, 3, 1];
 var bubbleSortSampleLong = [3, 4, 9, 3, 1, 10, 11, 40, 6, 4, 3, 1, 3, 4, 5, 8];
 // bubbleSort(bubble);
-console.log("Hello ", bubbleSortSample);
+console.log(bubbleSortSample);
 console.log(handleBubbleSort(bubbleSortSample));

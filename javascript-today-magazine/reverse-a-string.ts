d409f9dacd2d2f1e893fe9ef721a4d670eca7fdd
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-09-19 20:19:00 +0900

    init

---
+++ javascript-today-magazine/reverse-a-string.ts
@@ -0,0 +1,8 @@
+/**
+ * Reverse a string. It's a common interview question, and it's quite simple. 
+ * However, if you really want to impress your interviewer, you'd be well-off to understand multiple solutions to the problem. 
+ * Rather than using built-in methods like ".reverse()" (which is fine, but you may be asked to neglect using it), 👍
+ * demonstrate your ability to solve it in different ways.
+ * 
+ * Here are three solutions to reversing a string in JavaScript.👏
+ */

fcd3ed1b01e78adafd6070e5a774ea285b13ce30
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-09-20 11:50:39 +0900

    queue data structure init commit

---
+++ javascript-today-magazine/queue-data-structure.ts
@@ -0,0 +1,16 @@
+/**
+ * A Queue is a linear structure which follows a particular order in which the operations are performed. 
+ * The order is First In First Out (FIFO). 
+ * A good example of a queue would simply be a line at a grocery store. The customer in line (queue) first, will leave first. 
+ * 
+ * Here's a JavaScript implementation of a Queue data structure. 🔥
+ */
+
+// A Queue Data Structure
+
+class Queue {
+    private data = []
+    constructor() {
+        this.data = []
+    }
+}

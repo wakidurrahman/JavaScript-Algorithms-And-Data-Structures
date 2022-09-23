2e39e74576b8337b1a262e8601caece58c4785b0
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-09-23 14:58:53 +0900

    queue data structure implement

--- javascript-today-magazine/queue-data-structure.ts
+++ javascript-today-magazine/queue-data-structure.ts
@@ -13,4 +13,31 @@ class Queue {
     constructor() {
         this.data = []
     }
+
+    /**
+     * add
+     */
+    public add(record: number| string | never) {
+        this.data.unshift(record);
+    }
+
+    /**
+     * remove
+     */
+    public remove() {
+        return this.data.pop();
+    }
+
 }
+
+ // Driver code
+ const arr = ["G", "e", "e", "k", "s", "f", "o",
+ "r", "g", "e", "e", "k", "s"];
+
+ const q = new Queue();
+ q.add(1);
+ q.add(3);
+ q.add(4);
+ console.log(q);
+ q.remove();
+ console.log(q);

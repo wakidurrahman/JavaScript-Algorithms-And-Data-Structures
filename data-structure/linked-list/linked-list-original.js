37f26e3c0c741ba0a6b91a9a196a238cba997404
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-09-27 15:35:45 +0900

    fix(queue-28):  link list and linknode

---
+++ data-structure/linked-list/linked-list-original.js
@@ -0,0 +1,53 @@
+import LinkedListNode from "./linked-list-node-original";
+import Comparator from "../../utils/comparator-original";
+
+export default class LinkedList {
+  /**
+   * @param {Function} [comparatorFunction]
+   */
+  constructor(comparatorFunction) {
+    /** @var LinkedListNode */
+    this.head = null;
+    /** @var LinkedListNode */
+    this.tail = null;
+    this.compare = new Comparator(comparatorFunction);
+  }
+
+  /**
+   * @param {*} value
+   * @returns {LinkedList}
+   */
+  prepend(value) {
+    // Make new node to be a head.
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+
+    // If there is no tail yet let's make new node a tail.
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+    return this;
+  }
+
+  /**
+   *
+   * @param {*} value
+   * @returns {LinkedList}
+   */
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    // If there is no head yet let's make new node a head.
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+
+    // Attach new node to the end of linked list.
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+}
--- utils/comparator-original.js
+++ utils/comparator-original.js
@@ -1,8 +1,8 @@
 export default class Comparator {
   /**
    * Constructor.
-   * @param {function(a: *, b: *)} [compareFunction] - It may be custom compare function that, let's
-   * say may compare custom objects together.
+   * @param {function(a: *, b: *)} [compareFunction] - It may be custom compare function that, 
+   * let's say may compare custom objects together.
    */
 
   constructor(compareFunction) {

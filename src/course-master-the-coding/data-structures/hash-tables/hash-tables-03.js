9f4efdc0cfb96dccdec80053f0302e30cfde158d
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-12-01 11:42:25 +0900

    updated hash table

--- src/course-master-the-coding/data-structures/hash-tables/hash-tables-03.js
+++ src/course-master-the-coding/data-structures/hash-tables/hash-tables-03.js
@@ -1,43 +1,59 @@
 var HashTableMain = /** @class */ (function () {
-  function HashTableMain(size) {
-    this.data = [];
-    this.data = new Array(size);
-  }
-  HashTableMain.prototype.hash = function (key) {
-    var hash = 0;
-    for (var i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    function HashTableMain(size) {
+        this.data = [];
+        this.data = new Array(size);
     }
-    return hash;
-  };
-  /**
-   * set
-   */
-  HashTableMain.prototype.set = function (key, value) {
-    var address = this.hash(key);
-    if (!this.data[address]) {
-      this.data[address] = [];
-    }
-    this.data[address].push([key, value]);
-    return this.data;
-  };
-  /**
-   * Get
-   */
-  HashTableMain.prototype.get = function (key) {
-    var getAddress = this.hash(key);
-    var currentBucket = this.data[getAddress];
-    if (currentBucket) {
-      for (var i = 0; i < currentBucket.length; i++) {
-        if (currentBucket[i][0] === key) {
-          return currentBucket[i][1];
+    HashTableMain.prototype.hash = function (key) {
+        var hash = 0;
+        for (var i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
         }
-      }
-    }
-    return undefined;
-  };
-  return HashTableMain;
-})();
+        return hash;
+    };
+    /**
+     * set
+     */
+    HashTableMain.prototype.set = function (key, value) {
+        var address = this.hash(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value]);
+        return this.data;
+    };
+    /**
+     * Get
+     */
+    HashTableMain.prototype.get = function (key) {
+        var getAddress = this.hash(key);
+        var currentBucket = this.data[getAddress];
+        if (currentBucket) {
+            for (var i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
+    };
+    /**
+     * keys
+     */
+    HashTableMain.prototype.keys = function () {
+        var keysArray = [];
+        console.log(this.data.length);
+        for (var i = 0; i < this.data.length; i++) {
+            console.log(this.data[i]);
+            if (this.data[i]) {
+                console.log(this.data[i][0]);
+                console.log(this.data[i][0][0]);
+                keysArray.push(this.data[i][0][0]);
+            }
+        }
+        return keysArray;
+    };
+    return HashTableMain;
+}());
 // Example
 var myHashTable01 = new HashTableMain(50);
 myHashTable01.set('grapes', 10000);
@@ -46,5 +62,5 @@ myHashTable01.set('apples', 9);
 myHashTable01.get('apples');
 myHashTable01.set('banana', 'Nice foots');
 myHashTable01.get('banana');
-
+myHashTable01.keys();
 console.log(myHashTable01.data);

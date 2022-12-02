d090cd64ccf33436022221106231e6c5ca5129fd
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-12-02 15:22:19 +0900

    hash table question 01 solution 01 implement.

---
+++ src/course-master-the-coding/data-structures/hash-tables/hash-tables-04.ts
@@ -0,0 +1,27 @@
+// Google Question
+/**
+ * Given an array = [2, 5, 1, 2, 3, 5, 1, 2, 4]: It should return 2
+ *
+ * Given an array = [2, 1, 1, 2, 3, 5, 1, 2, 4]: It should return 1
+ *
+ * Given an array = [2, 3, 4, 5]: It should return undefined
+ *
+ */
+
+// Solution 01:
+function firstRecurringCharacter(array: number[]): number | undefined {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log('first : ', element);
+    for (let j = i + 1; j < array.length; j++) {
+      const element = array[j];
+      console.log('Second : ', element);
+      if (array[i] === array[j]) {
+        console.log(array[i]);
+        return array[i];
+      }
+    }
+  }
+  console.log('Nothing match : undefined');
+  return undefined;
+}

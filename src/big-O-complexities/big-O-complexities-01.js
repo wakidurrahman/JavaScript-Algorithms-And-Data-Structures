ee9488f7671255800581c737aaac2699a05f2cf5
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-12-06 16:38:15 +0900

    printQuadraticTime

---
+++ src/big-O-complexities/big-O-complexities-01.js
@@ -0,0 +1,35 @@
+/**
+ * Big-O complexities
+ *
+ * O(1) is constant time.
+ * O(1) does not change with respect to input space.
+ * Hence, O(1) is referred to as being constant time.
+ * An example of an O(1) algorithm is accessing an item in the array by its index.
+ *
+ * O(n) is linear time.
+ * O(n) is linear time and applies to algorithms that must do n operations in the worst-case scenario.
+ *
+ * O(n2) is quadratic time,
+ *
+ * O(n3) is cubic time.
+
+
+ */
+// O(n) algorithm is printing numbers from 0 to n-1
+function printLinerTime(n) {
+    for (var i = 0; i < n; i++) {
+        console.log(i);
+    }
+}
+console.log(printLinerTime(100));
+// O(n2) is quadratic time,
+function printQuadraticTime(n) {
+    for (var i = 0; i < n; i++) {
+        // parent loop
+        for (var j = 0; j < n; j++) {
+            // children loop
+            console.log(i, j);
+        }
+    }
+}
+console.log(printQuadraticTime(10));

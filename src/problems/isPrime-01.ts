be6d35c25449a305c6f33e2caf520a49c0fa67e4
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-12-12 15:19:36 +0900

    primeFactors

--- src/problems/isPrime-01.ts
+++ src/problems/isPrime-01.ts
@@ -35,3 +35,25 @@ function isPrimeTwo(n: number) {
   }
   return true;
 } // O(sqrt(n))
+
+function primeFactors(n: number) {
+  // Print the number of 2s that divide n
+  while (n % 2 == 0) {
+    console.log(2);
+    n = n / 2;
+  }
+
+  // n must be odd at this point. So we can skip one element
+  // note i = i + 2;
+  for (let i = 3; i * i <= n; i = i + 2) {
+    // While i divides n, print i and divide n
+    while (n % i == 0) {
+      console.log(i);
+      n = n / i;
+    }
+  }
+  // This condition is to handle the case when n is a prime number greater than 2
+  if (n > 2) {
+    console.log(n);
+  }
+} // O(sqrt(n))

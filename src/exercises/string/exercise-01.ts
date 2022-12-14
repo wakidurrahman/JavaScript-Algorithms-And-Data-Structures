305e4e34c3dd2438c9fae20471f19f2b878a1d67
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-12-14 11:15:41 +0900

    JavaScript String Primitive

--- src/exercises/string/exercise-01.ts
+++ src/exercises/string/exercise-01.ts
@@ -1,2 +1,15 @@
-let a = 'a';
-let b = 'b';
+/**
+ * JavaScript String Primitive
+ * JavaScript's native String primitive comes with various common string functions.
+ *
+ * 1. String Access
+ * 2. String Comparison
+ * 3. String Search
+ * 4. String Decomposition
+ * 5. String Replace
+ */
+
+// 1. String Access
+'Hello world'.charAt(1); // returns 'e';
+'YouTube'.substring(1, 2); // returns 'o' : multiple-character access.
+'YouTube'.substring(3, 7); // returns 'Tube'

e2fa5b0e62b9ba1227d0f2fbf912c744fa595dea
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-12-15 11:39:33 +0900

    Regular Expressions

---
+++ src/exercises/regular-expressions/regular-expressions-101.ts
@@ -0,0 +1,20 @@
+/**
+ * Regular Expressions
+ *
+ * Regular expressions (regexes) are a set of characters that define a search pattern.
+ *
+ * The constructor for the RegExp object takes two parameters:
+ * the regular expression and the optional match settings, as shown here:
+ *
+ * i ==> Perform case-insensitive matching
+ * g ==> Perform a global match (find all matches rather than stopping after first match)
+ * m ==> Perform multiline matching
+ *
+ * RegExp has the following two functions:
+ * 1. search(): Tests for matches in a string. This returns the index of the match.
+ * 2. match(): Tests for matches. This returns all the matches.
+ *
+ * The JavaScript String object also has the following two regex-related functions that accept the RegExp object as an argument:
+ * 1. exec(): Tests for matches in a string. This returns the first match.
+ * 2. test(): Tests for matches in a string. This returns true or false.
+ */

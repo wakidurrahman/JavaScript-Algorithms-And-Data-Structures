6acaeab934fe5b7d2e9578db6f6916796ef0399e
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-12-16 12:18:41 +0900

    shortening

---
+++ src/exercises/string/shortening.ts
@@ -0,0 +1,7 @@
+/**
+ * Have you ever wondered how URL-shortening sites such as Bit.ly work? A simplified URL compression algorithm follows a certain structure,
+ *
+ * For the shortening part, the following algorithm can be used.
+ * There are 62 possible letters and numbers,
+ * consisting of 26 lowercase letters, 26 uppercase letters, and 10 numbers (0 to 9).
+ */

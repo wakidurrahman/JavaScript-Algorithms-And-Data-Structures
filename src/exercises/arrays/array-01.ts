57d054e2f3fb1ab732a3127dc5eebf2739ada53e
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-12-27 12:50:50 +0900

    Deletion

--- src/exercises/arrays/array-01.ts
+++ src/exercises/arrays/array-01.ts
@@ -6,3 +6,13 @@ const array_insertion: number[] = [1, 2, 3, 4];
 array_insertion.push(5);
 array_insertion.push(7);
 array_insertion.push(2);
+
+// Deletion
+// This .pop() method removes the last-added element of the array.
+const array_deletion: number[] = [1, 2, 3, 4];
+array_deletion.pop();
+array_deletion.pop();
+
+// This .shift() method will remove the first element and return it.
+array_deletion.shift();
+array_deletion.shift();

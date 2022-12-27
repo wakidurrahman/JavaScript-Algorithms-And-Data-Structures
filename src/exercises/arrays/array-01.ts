cf8a4d82a88523010fd2b62f8db078c9ae014cd9
Wakidur Rahaman <wakidur.rahman@bjitgroup.com>
2022-12-27 12:42:21 +0900

    insertion

--- src/exercises/arrays/array-01.ts
+++ src/exercises/arrays/array-01.ts
@@ -1,2 +1,8 @@
 // Introducing Arrays
 const arrayInit: number[] = [1, 2, 3, 4];
+
+// insertion
+const array_insertion: number[] = [1, 2, 3, 4];
+array_insertion.push(5);
+array_insertion.push(7);
+array_insertion.push(2);

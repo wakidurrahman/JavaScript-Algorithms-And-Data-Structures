72c430eec947ccb04e65de1c9d9ff92c73535741
wakidurrahman <wakidur.rahman@bjitgroup.com>
2023-06-21 18:39:31 +0900

    feat: Sum of Array Plus One

--- src/code-challenges/code-challenges-002-010.js
+++ src/code-challenges/code-challenges-002-010.js
@@ -1,7 +1,57 @@
 /**
+ * 002: Lucky Sevens
+ * Problem: Write a function called lucky_sevens which takes an array of integers and returns true if any three consecutive elements sum to 7.
+ */
+
+function luckySevensVariationOne(arrayOfIntegers) {
+  // Array length
+  const len = arrayOfIntegers.length;
+  // If array of length is less than 3 elements then this challenge is not possible.
+  if (len < 3) {
+    return 'Not possible';
+  }
+
+  // Because we know there are at least 3 elements we can
+  // Start the loop at the 3rd element in the array (i = 2);
+  // and check it along with the two previous elements (i - 1) and (i - 2)
+
+  for (let i = 0; i < len; i++) {
+    if (arrayOfIntegers[i] + arrayOfIntegers[i - 1] + arrayOfIntegers[i - 2] === 7) {
+      return true;
+    }
+  }
+
+  // if loop is finished and no elements summed to 7;
+  return false;
+}
+
+luckySevensVariationOne([2, 1, 5, 1, 0]);
+
+function luckySevensVariationTwo(array) {
+  const len = array.length;
+
+  // Iterate through the array.
+
+  for (let i = 0; i < len - 2; i++) {
+    let sum = array[i] + array[i + 1] + array[i + 2];
+
+    if (sum === 7) {
+      return true; // Found three consecutive elements that sum to 3;
+    }
+  }
+
+  return false; // No three consecutive elements sum to 7;
+}
+
+let numbersOfLuckySeven = [1, 2, 3, 4, 5, 6, 1];
+console.log(luckySevensVariationTwo(numbers)); // Output: true
+
+var numbers2OfLuckySeven = [1, 2, 3, 4, 5, 6, 2];
+console.log(luckySevensVariationTwo(numbers2OfLuckySeven)); // Output: false
+
+/**
  * Q003: Simple clock angle
- * Problem
- * You will be given a number N that represents where the minute hand currently is on a clock.
+ * Problem: You will be given a number N that represents where the minute hand currently is on a clock.
  * Your program should return the angle that is formed by the minute hand and the 12 o'clock mark on the clock.
  */
 
@@ -15,8 +65,7 @@ function simpleClockAngle(number) {
 
 /**
  * Q004: Sum of several arrays
- * Problem
- * You will be given an array of several arrays that each contain integers and your goal is to write a function that will sum up all the numbers in all the arrays.
+ * Problem: You will be given an array of several arrays that each contain integers and your goal is to write a function that will sum up all the numbers in all the arrays.
  * For example, if the input is [[3, 2], [1], [4, 12]] then your program should output 22 because 3 + 2 + 1 + 4 + 12 = 22. Solve without and with reduce.
  */
 
@@ -45,7 +94,7 @@ multiDimensionalSumOfSeveralArrayVariationReduce([[3, 2], [1], [4, 12]]);
 
 /**
  * Q005: Test divisors of three
- * You will be given 2 parameters: a low and high number.
+ * Problem: You will be given 2 parameters: a low and high number.
  * Your goal is to print all numbers between low and high, and for each of these numbers print whether or not the number is divisible by 3.
  * If the number is divisible by 3, print the word "div3" directly after the number.
  */
@@ -71,7 +120,7 @@ testDivisors(2, 10);
 
 /**
  * Q006: Oddball sum
- * Write a function called oddball_sum which takes in a list of numbers and returns the sum of all the odd elements.
+ * Problem: Write a function called oddball_sum which takes in a list of numbers and returns the sum of all the odd elements.
  * Try to solve with and without reduce function.
  *
  */
@@ -106,3 +155,28 @@ function oddBallSumVariationReduce(numbs) {
 }
 
 oddBallSumVariationReduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+
+/**
+ * Q007: Sum of Array Plus One
+ *
+ * Problem: Write a function that takes an array of integers and returns the sum of the integers after adding 1 to each.
+ *
+ */
+
+// ES5 method is nice and clean
+exports.es5 = function (array) {
+  return array.reduce((memo, num) => {
+    return memo + num;
+  }, array.length);
+};
+
+// Without array.reduce method isn't much different
+exports.iterative = function (array) {
+  let result = array.length;
+
+  for (let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+
+  return result;
+};
